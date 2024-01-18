4d499359f46439e8ffeafae33871f1bc41f7baf1
Francis Poon <francool97@gmail.com>
2024-01-18 18:59:26 -0500

    LevelSelect: Level select buttons now generate dynamically depending on how many levels are on GameData

--- Assets/Scripts/LevelSelectManager.cs
+++ Assets/Scripts/LevelSelectManager.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelSelectManager : MonoBehaviour
 {
+    [Header("Prefabs")]
+    [SerializeField]
+    private GameObject levelSelectButton;
+
     [Header("Attributes")]
     [SerializeField]
+    private GameObject levelSelectGrid;
+
     private GameObject[] levelSelectButtons;
 
     private void Awake()
     {
-        for (int c = 0; c < levelSelectButtons.Length; c++)
+        levelSelectButtons = new GameObject[GameData.levels.GetLength(0)];
+    }
+
+    private void Start()
+    {
+        for (int c = 0; c < GameData.levels.GetLength(0); c++)
         {
+            levelSelectButtons[c] = Instantiate(levelSelectButton, levelSelectGrid.transform);
+            levelSelectButtons[c].GetComponentInChildren<TextMeshProUGUI>().text = $"{c + 1}";
+            int levelIndex = c;
+            levelSelectButtons[c].GetComponent<Button>().onClick.AddListener(() => SelectLevel(levelIndex));
+            levelSelectButtons[c].GetComponent<Button>().onClick.AddListener(StartGame);
             levelSelectButtons[c].GetComponent<Button>().interactable = c <= GameData.unlockedLevel;
         }
+        
     }
 
     public void SelectLevel(int level)
     {
+        Debug.Log($"Selecting level {level}");
         GameData.selectedLevel = level;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
